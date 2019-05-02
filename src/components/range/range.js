39c2f941b52ac8653f4e955e36369186e9ebd0c1
Aris_home <aref.ivan@gmail.com>
2019-05-03 00:21:38 +0300

    стилизовал range input  черзе ion-range

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -1,17 +1,20 @@
 import $ from 'jquery';
 // import jQuery from "jquery";
-import "../../../node_modules/ion-rangeslider/css/ion.rangeSlider.min.css" 
+// import "../../../node_modules/ion-rangeslider/css/ion.rangeSlider.min.css" 
 
 import "ion-rangeslider";
 
 $(document).ready(function(){
     $(".range").ionRangeSlider({
+        skin:"big",
         type: "double",
         min: 0,
         max: 1000,
         from: 200,
         to: 500,
-        grid: true
+        hide_from_to:true,
+        hide_min_max:true
+        
     })
     
 })

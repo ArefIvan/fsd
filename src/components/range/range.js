00f4c0b231e604a29450e2795b501d15d1b86350
Aris <aref.ivan@gmail.com>
2019-05-03 19:25:10 +0300

    Делаю бар

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -7,14 +7,41 @@ import $ from 'jquery';
 $(document).ready(function(){
     
     var inputs = $('.range__input');
-    var to = parseInt(inputs[0].value);
-    var from = parseInt(inputs[1].value);
+    var bar = $(".range__bar")
+    var to = inputs[0];
+    var from = inputs[1];
     var width = inputs.width();
     var max = inputs[0].max;
     var min = inputs[0].min;
-    inputs.change(function(){
-        console.log(to);
-        console.log(from);
+    inputs.input(function(){
+        let valTo = parseInt(to.value);
+        let valFrom = parseInt(from.value);
+        let percTo = (valTo-min)/((max-min)/100);
+        let percFrom = (valFrom-min)/((max-min)/100);
+
+
+        bar.css({
+            left:percTo + "%",
+            width:(percFrom - percTo) + "%",
+        })
+        if(this.value==valTo){            
+            if(valTo>=valFrom){
+                to.value = valFrom - ((max/100)*3);
+                return;
+        }
+        }
+        if(this.value==valFrom){
+            
+            if(valFrom<=valTo){
+                this.value= valTo + ((max/100)*3);
+               
+                return;
+        }
+        }
+        
+       
+        console.log(valTo)
+        console.log(valFrom)
     })
 ;    
 })

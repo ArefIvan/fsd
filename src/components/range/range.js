5b62093318ca6f4303fae9e0bfa106e57c8e3e4d
Aris <aref.ivan@gmail.com>
2019-05-03 17:10:11 +0300

    ваяю свой range на jquery

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -2,32 +2,19 @@ import $ from 'jquery';
 // import jQuery from "jquery";
 // import "../../../node_modules/ion-rangeslider/css/ion.rangeSlider.min.css" 
 
-import "ion-rangeslider";
+
 
 $(document).ready(function(){
-    var rangeFrom = $('.range__from');
-    var rangeTo = $(".range__to");
-    $(".range").ionRangeSlider({
-        skin:"big",
-        type: "double",
-        min: 0,
-        max: 1000,
-        from: 200,
-        to: 500,
-        hide_from_to:true,
-        hide_min_max:true
-        
-    })
-    $(".range").on("change", function () {
-        var $inp = $(this);
-        var v = $inp.prop("value");     // input value in format FROM;TO
-        var from = $inp.data("from");   // input data-from attribute
-        var to = $inp.data("to");       // input data-to attribute
-        console.log($inp);
-        rangeFrom.html(from);
-        rangeTo.html(to) ;     // all values
-
-
-    });
     
+    var inputs = $('.range__input');
+    var to = parseInt(inputs[0].value);
+    var from = parseInt(inputs[1].value);
+    var width = inputs.width();
+    var max = inputs[0].max;
+    var min = inputs[0].min;
+    inputs.change(function(){
+        console.log(to);
+        console.log(from);
+    })
+;    
 })

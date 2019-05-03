cc06d66b4c1b71b411efb673114b655c67d9003e
Aris <aref.ivan@gmail.com>
2019-05-03 15:21:58 +0300

    настраивал range

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -5,6 +5,8 @@ import $ from 'jquery';
 import "ion-rangeslider";
 
 $(document).ready(function(){
+    var rangeFrom = $('.range__from');
+    var rangeTo = $(".range__to");
     $(".range").ionRangeSlider({
         skin:"big",
         type: "double",
@@ -16,5 +18,16 @@ $(document).ready(function(){
         hide_min_max:true
         
     })
+    $(".range").on("change", function () {
+        var $inp = $(this);
+        var v = $inp.prop("value");     // input value in format FROM;TO
+        var from = $inp.data("from");   // input data-from attribute
+        var to = $inp.data("to");       // input data-to attribute
+        console.log($inp);
+        rangeFrom.html(from);
+        rangeTo.html(to) ;     // all values
+
+
+    });
     
 })

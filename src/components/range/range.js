e02126c45c59e30d9491ca00868986550969adb4
Aris <aref.ivan@gmail.com>
2019-05-04 19:50:25 +0300

    .проверка на наличее инпута

--- src/components/range/range.js
+++ src/components/range/range.js
@@ -8,25 +8,26 @@ $(document).ready(function(){
     var ran = $(".range-slide1") 
     var ran1=$(".range-slide2")
     range(ran)
-    // range(ran1)
+    range(ran1)
     
     function range(elem){
+        if(!(elem.children().is('.range__input'))){
+            
+            return;
+        }
         
         let inputs = elem.children('.range__input'); 
         let bar = elem.children(".range__bar")
         let from = inputs[0]; 
         let to = inputs[1];
         let max = inputs[0].max;
-        let min = inputs[0].min;
-        
-        
-        bar.css({
-            left:( ( from.value - min ) / ( (max - min) / 100 ) ) + "%",
-            width:( ( to.value - min ) / ( ( max - min ) / 100 ) ) - ( ( from.value - min ) / ( ( max - min ) / 100 ) )+"%",
+        let min = inputs[0].min;        
+    bar.css({
+        left:( ( from.value - min ) / ( (max - min) / 100 ) ) + "%",
+        width:( ( to.value - min ) / ( ( max - min ) / 100 ) ) - ( ( from.value - min ) / ( ( max - min ) / 100 ) )+"%",
     })
     inputs.on('input',function(){
-        let valFrom = parseInt(from.value);
-        
+        let valFrom = parseInt(from.value);       
         let valTo = parseInt(to.value);
         let percFrom= (valFrom-min)/((max-min)/100);
         let percTo = (valTo-min)/((max-min)/100);
@@ -34,8 +35,7 @@ $(document).ready(function(){
             left:(percFrom)?percFrom  + "%":"1%",
             width:((percTo - percFrom)<100)?(percTo - percFrom) + "%":"99%",
         })
-        if(this.value==valFrom){ 
-                       
+        if(this.value==valFrom){                    
             if(valFrom>valTo){
                 from.value = valTo - ((max/100)*2);
                 return;

cc77d98e54a6657c66cd30dabddda87ba9a13089
Aris_home <aref.ivan@gmail.com>
2019-05-06 21:31:25 +0300

    поправил шаблон range,написал шаблон скрипта

---
+++ src/components/script_ext.js
@@ -0,0 +1,16 @@
+import $ from 'jquery';
+
+
+function catalogItemSome(field){
+
+    let SOME = function(el){
+        // code
+    }
+
+    $(field).each(function(){
+        SOME($(this));
+    });
+
+}
+
+catalogItemSome("name_selector")

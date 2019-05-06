ad1148dee0b8466ba9a6e26244861723bd87dca7
Aris_home <aref.ivan@gmail.com>
2019-05-06 20:07:25 +0300

    сделал проверки в counter

--- src/components/counter/counter.js
+++ src/components/counter/counter.js
@@ -1,9 +1,10 @@
 import $ from 'jquery';
+ 
 function catalogItemCounter(field){
 			
-    var fieldCount = function(el) {
+    let fieldCount = function(el) {
         
-        var 
+        let 
             // Мин. значение
             min = el.data('min'),
             
@@ -15,23 +16,56 @@ function catalogItemCounter(field){
 
             // Кнопка увеличения кол-ва
             inc = el.next('.inc');
-            console.log(min)
+            
 
 
         function init(el) {
-            console.log(el)
+           
             if(!el.attr('disabled')){
                 dec.on('click', decrement);
                 inc.on('click', increment);
-                el.on("input",inpData)
+                el.on("input",checkInp);
+                el.on("change",checkChange)
             }
 
-            function inpData(){
-
+            function checkInp(){
+                let value = el[0].value
+                value =  value.split(/\D/).join("");//вырезаем все кроме цифр               
+                if(value>=max){
+                    value = max;
+                }
+                el[0].value = value                
+            }
+            function checkChange(){
+                let value = el[0].value
+              
+                if(value<=min||value==""){
+                    if(min==""){
+                        el[0].value=0
+                        dec.prop("disabled",true)
+                        inc.prop("disabled",false)
+                    }
+                    else{
+                    el[0].value=min
+                    dec.prop("disabled",true)
+                    inc.prop("disabled",false)
+                    }
+                }else{
+                    if(value>=max){
+                        inc.prop("disabled",true)
+                        dec.prop("disabled",false)
+                        el[0].value = max
+                    }
+                    else{
+                        inc.prop("disabled",false)
+                        dec.prop("disabled",false)
+                        el[0].value=value
+                    }    
+                 }  
             }
             // Уменьшим значение
             function decrement() {
-                var value = parseInt(el[0].value);
+                let value = parseInt(el[0].value);
                         
                     value--;
                     if(value < max){
@@ -43,17 +77,12 @@ function catalogItemCounter(field){
                     }
                     else{
                         el[0].value = value;
-                    }
-
-                   
-                    
-                   
-                
+                    }              
             };
 
             // Увеличим значение
             function increment() {
-                var value = parseInt(el[0].value);
+                let value = parseInt(el[0].value);
                     
                 value++;
                 if(value>min){

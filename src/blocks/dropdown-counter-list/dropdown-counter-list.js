31db945d4e7a80c117969df8d5d1e895f0645e03
aris_laptop <aref.ivan@gmail.com>
2019-05-25 12:58:06 +0300

    разделил counter-list и counter-plus

--- src/blocks/dropdown-counter-list/dropdown-counter-list.js
+++ src/blocks/dropdown-counter-list/dropdown-counter-list.js
@@ -4,35 +4,41 @@ import "../../components/dropdown/dropdown";
 
 let dropCountList = $(".dropdown--counter-list")
 
-let counts = dropCountList.find(".counter__count")
-let titleDrop = dropCountList.find(".field-dropdown__title")
-let dec=dropCountList.find(".dec")
-let inc=dropCountList.find(".inc")
-let titleCoutner=dropCountList.find(".counter__title")
-getTitleDrop()
+function countsList(el){
+        let counts = el.find(".counter__count")
+    let titleDrop = el.find(".field-dropdown__title")
+    let dec=el.find(".dec")
+    let inc=el.find(".inc")
+    let titleCoutner=el.find(".counter__title")
+    getTitleDrop()
 
 
-counts.each(function(i){   
-   $(dec[i]).on('click',function(){
-       setTimeout(getTitleDrop,10)
-        })
-        
-    $(inc[i]).on("click",function(){
+    counts.each(function(i){   
+    $(dec[i]).on('click',function(){
         setTimeout(getTitleDrop,10)
-    })
+            })
+            
+        $(inc[i]).on("click",function(){
+            setTimeout(getTitleDrop,10)
+        })
 
-})
-function getTitleDrop(){
-    let titleDropStr=""
-    let titleDropArr=[]
-    counts.each(function(i){
-    let str  = this.value + " " + titleCoutner[i].innerHTML+" "
-    titleDropArr.push(str)
-    titleDropStr=titleDropArr.join(",")
-    titleDrop.html(titleDropStr)
+    })
+    function getTitleDrop(){
+        let titleDropStr=""
+        let titleDropArr=[]
+        counts.each(function(i){
+        let str  = this.value + " " + titleCoutner[i].innerHTML+" "
+        titleDropArr.push(str)
+        titleDropStr=titleDropArr.join(",")
+        titleDrop.html(titleDropStr)
+        })
+    }
+    counts.each(function(){
+        
+        counter($(this))
     })
 }
-counts.each(function(){
-    
-    counter($(this))
+
+dropCountList.each(function(){
+    countsList($(this))
 })
--- src/blocks/dropdown-counter-plus/dropdown-counter-plus.js
+++ src/blocks/dropdown-counter-plus/dropdown-counter-plus.js
@@ -3,36 +3,58 @@ import "../../components/dropdown/dropdown";
 
 
 
-let dropCountList = $(".dropdown--counter-plus")
+let dropCountPlus = $(".dropdown--counter-plus")
 
-let counts = dropCountList.find(".counter__count")
-let titleDrop = dropCountList.find(".field-dropdown__title")
-let dec=dropCountList.find(".dec")
-let inc=dropCountList.find(".inc")
-let titleCoutner=dropCountList.find(".counter__title")
-getTitleDrop()
+ function countsPlus(el)   {
+    let counts = el.find(".counter__count")
+    let titleDrop = el.find(".field-dropdown__title")
+    let dec=el.find(".dec")
+    let inc=el.find(".inc")
+
+    getTitleDrop()
 
 
-counts.each(function(i){   
-   $(dec[i]).on('click',function(){
-       setTimeout(getTitleDrop,10)
-        })
-        
-    $(inc[i]).on("click",function(){
+    counts.each(function(i){   
+    $(dec[i]).on('click',function(){
         setTimeout(getTitleDrop,10)
-    })
+            })
+            
+        $(inc[i]).on("click",function(){
+            setTimeout(getTitleDrop,10)
+        })
 
-})
-function getTitleDrop(){
-    let titleDropStr="Гостей"
-    let sum = 0
-    
-    counts.each(function(i){
-        let sum =+ this.value
     })
-    titleDrop.html(sum+titleDropStr)
+    function getTitleDrop(){
+        
+        let sum = 0     
+        counts.each(function(){
+           
+            sum += +(this.value)
+           
+        })
+        titleDrop.html(sum+" "+checkExt("Гость",sum))
+    }
+    function checkExt(str,val){
+        if(str=="Гость"){
+            let root = "Гост"
+            let ext = ["ь","я","ей"]
+            if((val<10||val>20)&&val%10==1){
+                return root+ext[0]
+            }else{
+                if((val<10||val>20)&&(val%10>=2&&val%10<=4)){
+                    return root+ext[1]
+                }else{
+                    return root+ext[2]
+             }
+            }
+        }
+    }
+
+    counts.each(function(){
+        
+        counter($(this))
+    })
 }
-counts.each(function(){
-    
-    counter($(this))
+dropCountPlus.each(function(){
+    countsPlus($(this))
 })

b5398a9edd88b49ce618d788a0ba31b54975c740
aris_laptop <aref.ivan@gmail.com>
2019-06-07 17:24:14 +0300

    Сделал окончание слов

--- src/blocks/dropdown-counter-list/dropdown-counter-list.js
+++ src/blocks/dropdown-counter-list/dropdown-counter-list.js
@@ -1,7 +1,25 @@
 import counter from  "../../components/counter/counter";
 import "../../components/dropdown/dropdown";
+import { workerData } from "worker_threads";
 
-
+let dist=[
+    {word:"спальни",
+    root:"спал",
+    ext:["ьня","ьни","ен"]
+    },
+    {word:"кровати",
+    root:"кроват",
+    ext:["ь","и","ей"]
+    },
+    {word:"ванные",
+    root:"ванн",
+    ext:["ая","ых","ых"]
+    },
+    {word:"комнаты",
+    root:"комнат",
+    ext:["а","ы",""]
+    }
+];
 let dropCountList = $(".dropdown--counter-list")
 
 function countsList(el){
@@ -33,7 +51,8 @@ function countsList(el){
         let asseptFlag=false;
         let cancelFlag=false;
         counts.each(function(i){
-            let str  = this.value + " " + titleCoutner[i].innerHTML+" ";
+
+            let str  = this.value + " " + checkExt(titleCoutner[i].innerHTML,this.value)+" ";
 
             titleDropArr.push(str);
             titleDropStr=titleDropArr.join(",");
@@ -41,10 +60,35 @@ function countsList(el){
             if(this.value!=0){asseptFlag=true}
             if(this.value!=$(this).data("min")){cancelFlag=true}            
         })
-        if(asseptFlag)assept.show()
-            else assept.hide()
-        if(cancelFlag)cancel.show()
-            else cancel.hide()
+        if(asseptFlag)assept.css("opacity",1)
+            else assept.css("opacity",0)
+        if(cancelFlag)cancel.css("opacity",1)
+            else cancel.css("opacity",0)
+    }
+    function checkExt(str,val,dist){
+        function ext(root,ext){
+            if((val<10||val>20)&&val%10==1){
+                return root+ext[0]
+            }else{
+                if((val<10||val>20)&&(val%10>=2&&val%10<=4)){
+                    return root+ext[1]
+                }else{
+                    return root+ext[2]
+             }
+            }
+        }
+        if(str=="спальни"){
+           return ext("спал",["ьня","ьни","ен"])          
+        }
+        if(str=="кровати"){
+            return ext("кроват",["ь","и","ей"])
+        }
+        if(str=="ванные комнаты"){
+            
+          return ext("ванн",["ая","ых","ых"]) + " " + ext("комнат",["а","ы",""])
+          
+        }
+                
     }
     counts.each(function(){
         

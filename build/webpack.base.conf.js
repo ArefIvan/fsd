2ec360649dfdd0310f84520842f9c9b3fd51853b
Aris <aref.ivan@gmail.com>
2019-06-11 13:13:26 +0300

    cards

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -118,6 +118,11 @@ module.exports = {
       template: `${PATHS.src}/form.pug`,
       filename: './form.html'
     }),
+    new HtmlWebpackPlugin({
+      hash: false,
+      template: `${PATHS.src}/cards.pug`,
+      filename: './cards.html'
+    }),
     new CopyWebpackPlugin([
     //  { from: `${PATHS.src}/img/`, to: `${PATHS.assets}img` },
       { from: `${PATHS.src}/static`, to: '' },

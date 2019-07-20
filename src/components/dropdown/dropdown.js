96183e2927f6a73fd5954803d3132abe3b5eb23b
Aris <aref.ivan@gmail.com>
2019-07-20 20:35:40 +0400

    Сделал две карточки

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -5,10 +5,16 @@ function catalogItemDropdown(field){
 
         let btn = el.find(".field-dropdown__icon")
         let content = el.children('.field-dropdown__content')
+        let assept = el.find(".assept")
         content.hide();
         btn.on("click",function(){
             el.toggleClass("field-dropdown--expand")
             content.stop(true).slideToggle()
+        })
+        assept.on('click',function(e){
+            e.preventDefault();
+            console.log(btn)
+            btn[0].click();
         })
     };   
     $(field).each(function(){

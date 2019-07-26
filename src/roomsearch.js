395d7b643121ca8bf89f18e2fd06f5d0161394c8
Aris <aref.ivan@gmail.com>
2019-07-26 15:36:52 +0300

    roomsearch pagination

--- src/roomsearch.js
+++ src/roomsearch.js
@@ -1,19 +1,18 @@
 import "./js/jquery.touchSwipe.min.js"
 let filter = $(".filterroom")
 let drop = $(".filterroom__drop")
+let rooms = $(".roomsearch__rooms")
+console.log(1)
 
-drop.swipe({
-    swipe:function(event, direction, distance, duration, fingerCount, fingerData){        
+rooms.swipe({
+
+    swipeOpen:function(event, direction, distance, duration, fingerCount, fingerData){        
         let filterWidht=filter.width()
         let dropWidht=drop.width()
+        console.log(12)
         if(direction=="right"){
             filter.animate({
                 left:0
-            })
-        }
-        if(direction=="left"){
-            filter.animate({
-                left:"-"+(filterWidht-dropWidht)+"px"
             })
         }
     }
@@ -21,3 +20,16 @@ drop.swipe({
 }
 
 )
+// drop.swipe({
+//     swipeClose:
+//     function(event, direction, distance, duration, fingerCount, fingerData){        
+//         let filterWidht=filter.width()
+//         let dropWidht=drop.width()
+
+//         if(direction=="left"){
+//             filter.animate({
+//                 left:"-"+(filterWidht-dropWidht)+"px"
+//             })
+//         }
+//     }
+// })
